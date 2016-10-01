6f0e77ab13f264a79a4ada377d71709dea9a0988
Abhishek Gautam <agautam2@buffalo.edu>
2016-09-30 23:25:34 -0400

    Navigation Drawer, Menu, Icons, Titles

---
+++ app/src/main/java/ubcomputerscience/ubwins/navigationexample/HandleTwo.java
@@ -0,0 +1,21 @@
+package ubcomputerscience.ubwins.navigationexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Gautam on 9/30/16.
+ * MBP111.0138.B16
+ * agautam2@buffalo.edu
+ * University at Buffalo, The State University of New York.
+ * Copyright © 2016 Gautam. All rights reserved.
+ */
+
+public class HandleTwo extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two);
+    }
+}
